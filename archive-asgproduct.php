7329ed4d82c8869576946cb894a3fd8ae10710d4
Ruvick <rudikov.web@ya.ru>
2022-03-15 11:17:53 +0300

    Блок сортировки в темплейт

--- archive-asgproduct.php
+++ archive-asgproduct.php
@@ -21,35 +21,10 @@ get_header('page'); ?>
 
 <section id="catalog-sec" class="catalog-sec">
   <div class="container">
-
 	<div class="catalog-sec__wrap">
 		<?php get_template_part('template-parts/sidebar-catalog');?>
     <div class="catalog-sec__product">
-
-				<div class="catalog-sec__sorting">
-          <div class="catalog-sec__sorting-name">Сортировать по:</div>
-          <form action="#" class="catalog-sec__sorting-select-form form">
-    				<div class="catalog-sec__options options">
-      				<label class="catalog-sec__options-item options__item">
-        				<input class="catalog-sec__options-input options__input" checked="" type="radio" value="1" name="form[option]">
-        				<span class="catalog-sec__options-text options__text"><span>Популярности </span></span> 
-      				</label>
-      				<label class="catalog-sec__options-item options__item">
-        				<input class="catalog-sec__options-input options__input" type="radio" value="2" name="form[option]">
-        				<span class="catalog-sec__options-text options__text"><span>Цене</span></span>
-      				</label>
-      				<label class="catalog-sec__options-item options__item">
-        				<input class="catalog-sec__options-input options__input" type="radio" value="3" name="form[option]">
-        				<span class="catalog-sec__options-text options__text"><span>Скидке</span></span>
-      				</label>
-      				<label class="catalog-sec__options-item options__item">
-        				<input class="catalog-sec__options-input options__input" type="radio" value="3" name="form[option]">
-        				<span class="catalog-sec__options-text options__text"><span>Обновлению</span></span>
-      				</label>
-    				</div>
-          </form>
-        </div>
-
+			<?php get_template_part('template-parts/sorting-block');?>
     	<div class="catalog-sec__row">
         <?php 
           while(have_posts()):
@@ -58,16 +33,13 @@ get_header('page'); ?>
         ?>
         <?php endwhile;?>
       </div>
-
     </div>
-
 		</div>
 		<div class="pagination">
 			<?php the_posts_pagination(array('prev_text' => '«', 'next_text' => '»'));?>
 		</div>
   </div> 
 </section>
-
 </main>
 
 
---
+++ template-parts/sorting-block.php
@@ -0,0 +1,23 @@
+<div class="catalog-sec__sorting">
+  <div class="catalog-sec__sorting-name">Сортировать по:</div>
+  <form action="#" class="catalog-sec__sorting-select-form form">
+    <div class="catalog-sec__options options">
+      <label class="catalog-sec__options-item options__item">
+        <input class="catalog-sec__options-input options__input" checked="" type="radio" value="1" name="form[option]">
+        <span class="catalog-sec__options-text options__text"><span>Популярности </span></span> 
+      </label>
+      <label class="catalog-sec__options-item options__item">
+        <input class="catalog-sec__options-input options__input" type="radio" value="2" name="form[option]">
+        <span class="catalog-sec__options-text options__text"><span>Цене</span></span>
+      </label>
+      <label class="catalog-sec__options-item options__item">
+        <input class="catalog-sec__options-input options__input" type="radio" value="3" name="form[option]">
+        <span class="catalog-sec__options-text options__text"><span>Скидке</span></span>
+      </label>
+      <label class="catalog-sec__options-item options__item">
+        <input class="catalog-sec__options-input options__input" type="radio" value="3" name="form[option]">
+        <span class="catalog-sec__options-text options__text"><span>Обновлению</span></span>
+      </label>
+    </div>
+  </form>
+</div>
