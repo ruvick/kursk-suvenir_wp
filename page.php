166b7ff304d3308aa091690a77b124d1e8dd5d94
Ruvick <rudikov.web@ya.ru>
2022-03-17 01:55:48 +0300

    Правки на мобилке

--- page.php
+++ page.php
@@ -12,25 +12,20 @@
  * @package souvenir
  */
 
-get_header('page'); ?>
+get_header(); ?>
 
 <section id="cardProduct" class="cardProduct">
   <div class="container">
 	<?php
 		if ( function_exists('yoast_breadcrumb') ) {
-			yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+			yoast_breadcrumb( '<p id="breadcrumbs">','</p>' ); 
 		}
 	?>
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			get_template_part( 'template-parts/content', 'page' );
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-		endwhile; // End of the loop.
-		?>
+<h1 class="single-section-title section-title"><span><?php echo the_title();?></span></h1>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content();?>
+					<?php endwhile;?>
+				<?php endif; ?> 
 	</div> 
 </section>
 
